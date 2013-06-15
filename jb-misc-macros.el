e1ff18c2abc7de01a58a591b9d00745035e7ece6
Joe Bloggs <vapniks@yahoo.com>
2013-06-15 20:59:34 +0100

    New macro: read-key-menu and function number-list
    Altered items:
    number-list, read-key-menu, jb-misc-macros

--- jb-misc-macros.el
+++ jb-misc-macros.el
@@ -84,7 +84,7 @@
 
 ;;; TODO
 ;;
-;; More macros.
+;; Macro that works like apply-partially but allowing partial application of args in any position.
 ;;
 
 ;;; Require
@@ -115,6 +115,70 @@ between successive evaluations of NEXTFORM."
 (defmacro list-subset (indices list)
   "Return elements of LIST corresponding to INDICES."
   `(mapcar (lambda (i) (nth i ,list)) ,indices))
+
+(defun number-list (start end &optional length)
+  "Return a sequential list of numbers from START to END.
+If END is nil and LENGTH is provided then return a list from START to (1- (+ START LENGTH))."
+  (if end
+      (loop for i from start to end collect i)
+    (assert length)
+    (loop for i from start to (1- (+ start length)) collect i)))
+
+;; This might be better as a function but I wanted to practice writing macros.
+;; Also this way we can use gensyms to minimize the number of variables bound in the let
+;; form surrounding the evaluation of FORMS.
+(defmacro* read-key-menu (prompts forms &optional startstr endstr keys)
+  "Prompt the user for a key and return the results of evaluating the corresponding form in the list FORMS.
+If KEYS is supplied then it should be a list (of the same length as PROMPTS & FORMS) of keys to be prompted for.
+Otherwise the lowercase letters of the alphabet will be used.
+
+The prompt string for `read-key-sequence' will be formed from PROMPTS, KEYS and the optional STARTSTR and ENDSTR in
+the following way:
+
+STARTSTR
+K1) PROMPT1
+K2) PROMPT2
+...
+KN) PROMPT3
+ENDSTR
+
+Where K1-KN are key descriptions of the keys in KEYS, and PROMPT1-PROMPTN are the corresponding prompt strings in the
+list PROMPTS.
+
+The macro arguments will be evaluated once before expanding the macro."
+  (setq prompts (eval prompts) forms (eval forms) startstr (eval startstr) endstr (eval endstr))
+  (assert (= (length prompts) (length forms)))
+  (setq keys (let* ((origkeys (eval keys))
+                    (uniqkeys (remove 'nil origkeys))
+                    (nextkey 47))
+               (mapcar (lambda (key)
+                         (or key (progn
+                                   (setq nextkey (1+ nextkey))
+                                   (while (member (char-to-string nextkey) uniqkeys)
+                                     (setq nextkey (1+ nextkey)))
+                                   (char-to-string nextkey))))
+                       (nconc origkeys (make-list (- (length prompts) (length origkeys)) nil)))))
+  (assert (= (length prompts) (length keys)))
+  (with-gensyms (newprompts prompt retval keystrs maxlen)
+                (let* ((keystrs (mapcar 'key-description keys))
+                       (maxlen (loop for keystr in keystrs maximize (length keystr)))
+                       (newprompts (mapcar* (lambda (k p)
+                                              (let ((len (- maxlen (length k))))
+                                                (concat k ") " (make-string len ? ) p)))
+                                            keystrs prompts))
+                       (prompt (concat (and startstr (concat startstr "\n"))
+                                       (mapconcat 'identity newprompts "\n")
+                                       "\nC-g) Quit"
+                                       (and endstr (concat "\n" endstr)))))
+                  `(let ((,retval 'again))
+                     (while (eq ,retval 'again)
+                       (let (key)
+                         (while (not (or (member key ',keys) (equal key "")))
+                           (setq key (read-key-sequence ,prompt nil t nil t)))
+                         (if (equal key "")
+                             (keyboard-quit)
+                           (setq ,retval (eval (nth (position key ',keys :test 'equal) ',forms))))))
+                     ,retval))))
 
 (provide 'jb-misc-macros)
 
